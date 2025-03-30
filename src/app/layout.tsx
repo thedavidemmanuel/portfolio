6de50573e13de46998ec59001ed29bf848e94bfa
thedavidemmanuel <thedavidemmanuel@gmail.com>
2025-03-30 05:14:08 +0200

    Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Analytics } from "@vercel/analytics/react";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-XT4F77DN0H" 
+          strategy="afterInteractive" 
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XT4F77DN0H');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} transition-colors duration-300`}>
         <Header />
         {children}
